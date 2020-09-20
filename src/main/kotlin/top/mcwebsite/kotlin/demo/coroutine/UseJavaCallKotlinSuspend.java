56bff4cd8f0eafc0011bb74ca5dee6c9f59d1c0a
woxin123 <1293141942@qq.com>
2020-09-20 10:54:20 +0800

    kotlin coroutine 2020-09-20

---
+++ src/main/kotlin/top/mcwebsite/kotlin/demo/coroutine/UseJavaCallKotlinSuspend.java
@@ -0,0 +1,26 @@
+package top.mcwebsite.kotlin.demo.coroutine;
+
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.EmptyCoroutineContext;
+import org.jetbrains.annotations.NotNull;
+
+public class UseJavaCallKotlinSuspend {
+
+    public static void main(String[] args) throws InterruptedException {
+        Object result = SuspendFunctionKt.suspendFunc02("a", "b", new Continuation<Integer>() {
+            @NotNull
+            @Override
+            public CoroutineContext getContext() {
+                return EmptyCoroutineContext.INSTANCE;
+            }
+
+            @Override
+            public void resumeWith(@NotNull Object o) {
+                System.out.println(o);
+            }
+        });
+        System.out.println(result);
+        Thread.sleep(1000);
+    }
+}
